38653c45415e734d0ffa908c0f9d2f353c91e159
Lee Seung Hun <negrat@naver.com>
2022-01-10 15:52:14 +0900

    검색 결과에 따른 리턴 작성
    - HTTP 응답으로 동작

--- app/api/user/user.py
+++ app/api/user/user.py
@@ -1,3 +1,6 @@
+from pymysql import connect
+from pymysql.cursors import DictCursor
+
 # 사용자 정보 관련된 기능들을 모아두는 모듈
 # app.py에서 이 함수들을 끌어다 사용
 
@@ -13,16 +16,35 @@ def user_test():
     }
 
 def login_test(id, pw):
-    # 아이디 : admin / 비번 : qwer 라고 하면, 로그인 성공 응답.
-    # 그 외 : 실패 처리
+    # id, pw를 이용해서 -> SQL 쿼리 작성 -> 결과에 따라 다른 응답 리턴
     
-    if id == 'admin' and pw == 'qwer':
+    db = connect(
+        host='finalproject.cbqjwimiu76h.ap-northeast-2.rds.amazonaws.com',
+        port=3306,
+        user='admin',
+        passwd='Vmfhwprxm!123',
+        db='test_phone_book',
+        charset='utf8',
+        cursorclass=DictCursor
+    )
+    
+    cursor = db.cursor()
+    
+    sql = f"SELECT * FROM users WHERE email='{id}' AND password='{pw}'"
+    cursor.execute(sql)
+    
+    query_result = cursor.fetchone()
+    
+    # 쿼리 결과 : None -> 아이디 비번 맞는 사람 X -> 로그인 실패.
+    
+    if query_result == None:
         return {
-            'code' : 200,
-            'message' : 'login ok'
-        }
-    else:
-        return {
-            'code' : 400,
-            'message' :  'id or pw incorrect'
+            'code': 400,
+            'message': '아이디 또는 비밀번호가 잘못되었습니다.'
         }, 400
+    else:
+        # 검색 결과가 있다 => 아이디/비번 모두 맞는 사람 O -> 로그인 성공
+        return {
+            'code': 200,
+            'message': '로그인 성공'
+        }
