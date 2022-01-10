02a47f7322d3fae7ad5533b0bf2776f3504b4787
Lee Seung Hun <negrat@naver.com>
2022-01-10 10:16:54 +0900

    HTML 관련 주석 추가

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.templating import render_template
 
 def create_app():
     # 플라스크 서버를 변수에 담자.
@@ -14,6 +15,10 @@ def create_app():
     @app.route("/test") # 서버의 /test 주소로 오면 수행해줄 일을 작성
     def test():
         return "이곳은 테스트 페이지입니다."
+    
+    @app.route("/web")
+    def web_test():
+        return render_template('web_test.html') # templates 폴더 내부의 파일을 불러주는 역할.
     
     # 이 서버를 사용하도록 결과로 내보내자.
     return app
