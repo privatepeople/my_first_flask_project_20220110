91f9644a3b0c8e644e9a5d13cbe35f77c3cc000d
Lee Seung Hun <negrat@naver.com>
2022-01-10 10:06:00 +0900

    메인주소로 오면 Hello World 문장 보여주기

--- app/app.py
+++ app/app.py
@@ -6,4 +6,9 @@ def create_app():
     
     # 서버에 대한 세팅 진행
     
+    @app.route("/") # 만들고있는 서버의 / (아무것도 안붙인 주소) 로 접속하면 보여줄 내용
+    def test():
+        return "<h1>Hello World!!<h1>" # Hello World 문장 리턴 => 이 내용을 사용자에게 보여주겠다.
+    
+    # 이 서버를 사용하도록 결과로 내보내자.
     return app
