800d7f47bf8a2bdd665eea1d53e66a506e5577fc
Lee Seung Hun <negrat@naver.com>
2022-01-12 10:18:04 +0900

    DB에 연락 추가 전에, 사용자 ID값 사전검사

--- app/api/contact/contact.py
+++ app/api/contact/contact.py
@@ -17,6 +17,19 @@ cursor = db.cursor()
 
 
 def add_contact_to_db(params):
+    
+    # 사전 검사 : user_id 파라미터의 값이, 실제 사용자 id가 맞는지? 그런 사용자 있는지?
+    sql = f"SELECT * FROM users WHERE id = {params['user_id']}"
+    cursor.execute(sql)
+    
+    user_result = cursor.fetchone()
+    
+    if user_result == None:
+        return {
+            'code': 400,
+            'message': '해당 사용자 id값은 잘못되었습니다.'
+        }, 400
+    
     # 연락처 추가 등록 쿼리
     sql = f"INSERT INTO contacts (user_id, name, phone_num, memo) VALUES ('{params['user_id']}', '{params['name']}', '{params['phone']}', '{params['memo']}'"
     
