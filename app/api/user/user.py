59e5034689e27a93e0009d1de2b43d54dda6a305
Lee Seung Hun <negrat@naver.com>
2022-01-12 09:40:57 +0900

    아이디 / 비밀번호 별도 검사

--- app/api/user/user.py
+++ app/api/user/user.py
@@ -30,17 +30,36 @@ def user_test():
 def login_test(id, pw):
     # id, pw를 이용해서 -> SQL 쿼리 작성 -> 결과에 따라 다른 응답 리턴
     
+    # 이메일부터 있는지 검사. => 이메일이 없다면 -> "존재하지 않는 이메일입니다." message string으로 담아서 리턴. (400)
+    
+    # 이메일이 있다면? 추가 검사.
+    # 비밀번호도 맞는지 추가 검사 => 비밀번호가 맞다 : 200 - 누가 록인 했는지. (지금처럼)
+    # => 비밀번호가 틀리다 : 이메일은 존재하는데, 비밀번호만 틀리다 -> "비밀번호가 틀렸습니다." message string으로 담아서 리턴. (400)
+    
+    sql = f"SELECT * FROM users WHERE email = '{id}'"
+    cursor.execute(sql)
+    
+    query_result = cursor.fetchone()
+    
+    if query_result == None:
+        # 아이디부터 존재하지 않는 상황
+        return {
+            'code': 400,
+            'message': '존재하지 않는 이메일입니다.'
+        }, 400
+    
     sql = f"SELECT * FROM users WHERE email='{id}' AND password='{pw}'"
     cursor.execute(sql)
     
     query_result = cursor.fetchone()
     
-    # 쿼리 결과 : None -> 아이디 비번 맞는 사람 X -> 로그인 실패.
+    # 쿼리 결과 : None -> 아이디가 틀린 경우 -> 이전의 return으로 결과가 나갔을 것임..
+    # 지금의 None: 아이디는 있지만, 비번이 틀린걸로 간주
     
     if query_result == None:
         return {
             'code': 400,
-            'message': '아이디 또는 비밀번호가 잘못되었습니다.'
+            'message': '비밀번호가 잘못되었습니다.'
         }, 400
     else:
         # 검색 결과가 있다 => 아이디/비번 모두 맞는 사람 O -> 로그인 성공
@@ -85,13 +104,38 @@ def sign_up(params):
             'message': '이미 사용중인 이메일입니다.'
         }, 400
     
+    # 닉네임이 사용 중인가? 사용중이라면: code - 400, message - '이미 사용중인 닉네임입니다.'
+    sql = f"SELECT * FROM users WHERE nickname = '{params['nick']}'"
+    
+    cursor.execute(sql)
+    nickname_check_result = cursor.fetchone()
+    
+    if  nickname_check_result:
+        return {
+            'code': 400,
+            'message': '이미 사용중인 닉네임입니다.'
+        }, 400
+    
+    
     sql = f"INSERT INTO users (email, password, nickname) VALUES ('{params['email']}', '{params['pw']}', '{params['nick']}'); "
     
-    print(f'완쉉된 쿼리 : {sql}')
+    print(f'완성된 쿼리 : {sql}')
     
     cursor.execute(sql)
     db.commit()
     
+    sign_up_user_sql = f"SELECT * FROM users ORDER BY id DESC LIMIT 1;"
+    cursor.execute(sign_up_user_sql)
+    sign_up_user = cursor.fetchone()
+    
     return {
-        'test': 'test'
+        'code': 200,
+        'message': '회원가입 성공',
+        'data': {
+            'user': {
+                'id': sign_up_user['id'],
+                'email': sign_up_user['email'],
+                'nickname': sign_up_user['nickname']
+            }
+        }
     }
